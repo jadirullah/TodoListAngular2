f441677822a35de619ea83552164c057572ebae8
jadir <jadirullah@gmail.com>
2018-03-23 18:16:11 +0700

    todo list app with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
 
+interface Task{
+	 title: string,
+	 is_canceled: boolean
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'To Do List App With Angular';
+
+  tasks : Array<Task>=[
+  	{
+  		title :"Ngaji",
+  		is_canceled :false
+  	},
+  	{
+  		title :"Ngoding",
+  		is_canceled :false
+  	},
+  	{
+  		title :"Jalan-Jalan",
+  		is_canceled :false
+  	}
+  ];
+
+  clearTodo(){
+  	let do_delete =confirm("Are you sure to delete the task ? ");
+  	if(do_delete){
+  		this.tasks.splice(0);
+  	}
+  }
+
+  addTask(value: string){
+  	this.tasks.push(
+	  	{
+	  		title:value,
+	  		is_canceled: false
+	  	}
+  	)
+  }
+
+  cancelTask(idx: number){
+
+  	console.log(idx);
+
+  	if(this.tasks[idx].is_canceled){
+  		this.tasks[idx].is_canceled=false;
+  	}else{
+  		this.tasks[idx].is_canceled=true;
+  	}
+  }
+
+  deleteTask(idx: number){
+  	let do_delete =confirm("Are you sure to delete the task ? ");
+  	if(do_delete){
+  		this.tasks.splice(idx, 1);
+  	}
+  }
+
+
 }
